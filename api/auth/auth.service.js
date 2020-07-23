e88075ba6bf264780658d0eb393e6a7151dd29fd
Dor Kuperman <dorkup@gmail.com>
2020-07-23 17:08:30 +0300

    fjhfcj

--- api/auth/auth.service.js
+++ api/auth/auth.service.js
@@ -18,11 +18,8 @@ async function login(email, password) {
 
 async function signup(username, email, imgURL, isAdmin, isGoogle, password, goingPartys) {
     // logger.debug(`auth.service - signup with email: ${email}, username: ${username}`)
-<<<<<<< HEAD
     console.log('creds in auth service:', username, email, imgURL, isAdmin, isGoogle, password, goingPartys);
-=======
     // console.log('creds in auth service:', username, email, imgURL, isAdmin, isGoogle, password);
->>>>>>> 81b15302705685f3ee7f88f92bb5efa7cab2144a
     if (!isGoogle) {
         if (!email || !password || !username) return Promise.reject('email, username and password are required!')
         const hash = await bcrypt.hash(password, saltRounds)
