05fb7c85f956cd361f7525587e5e50f3aa9d622e
shayelbaz1 <shaysha6@gmail.com>
2020-07-26 23:57:26 +0300

    party times

--- api/party/party.routes.js
+++ api/party/party.routes.js
@@ -10,7 +10,7 @@ router.put('/addReview', addPartyReview)
 router.get('/:id', getParty)
 router.post('/', requireAuth, addParty)
 // Delete requireCreator because liked and going not working
-router.put('/:id', updateParty,)
+router.put('/:id', updateParty)
 router.delete('/:id', deleteParty)
 
 module.exports = router
